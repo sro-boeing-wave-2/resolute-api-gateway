ae81a8d960e5c3e540de58e6bca36e44a0714875
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-09-20 11:45:56 +0530

    changed ip to localhost

---
+++ AuthDecode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class AuthDecode
+{
+	
+        long agentid;
+        string name;
+        string profileimageurl;
+        long organisationid;
+        string departmentname;
+        string organisationname;
+
+    public long Agentid { get => agentid; set => agentid = value; }
+    public string Name { get => name; set => name = value; }
+    public string Profileimageurl { get => profileimageurl; set => profileimageurl = value; }
+    public long Organisationid { get => organisationid; set => organisationid = value; }
+    public string Departmentname { get => departmentname; set => departmentname = value; }
+    public string Organisationname { get => organisationname; set => organisationname = value; }
+}
