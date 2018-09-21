926ba25baa2c735fa4aec1676d4731184cbd609b
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-09-21 19:49:12 +0530

    allowed access to signup process

--- ApiGateway/Authentication.cs
+++ ApiGateway/Authentication.cs
@@ -60,7 +60,11 @@ namespace ApiGateway
             }
             else
             {
-                if (httpContext.Request.Path.ToString() != "/login")
+                if (httpContext.Request.Path.ToString() != "/login" ||
+                    httpContext.Request.Path.ToString() != "/upload"||
+                    httpContext.Request.Path.ToString() != "/signup"||
+                    httpContext.Request.Path.ToString() != "/enduser"||
+                    httpContext.Request.Path.ToString() != "/agents")
                 {
                     httpContext.Response.Headers.Add("error", "NotAuthorised");
                     httpContext.Response.StatusCode = 401;
