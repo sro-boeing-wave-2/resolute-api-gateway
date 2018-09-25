b1c945acd8309cb2c031a89df0108353514c29f1
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-09-25 10:07:41 +0530

    added inter communication

--- ApiGateway/Authentication.cs
+++ ApiGateway/Authentication.cs
@@ -19,65 +19,75 @@ namespace ApiGateway
 
         public async Task InvokeAsync(HttpContext httpContext)
         {
-            if (httpContext.Request.Headers.ContainsKey("token"))
+            if (httpContext.Request.Headers["Access"].ToString() != "Allow_Service")
             {
-               // string publickey = "<RSAPublicKey><Modulus>5QooqrD6rqxdXTH7XxLYm0rF2uLFTZZAqv1lMcRvNEr7kmvkNEN+NH6soK1/1v5DI6xHwKAHKyi323XrjH7jIrkjHl5RF09nicutCvL0cFiwSCTfWzmIwbAI3z4J09b2lzml44IautLjk18LnRM4fZbpZdzmkd/UmvgOzREvxUU=</Modulus><Exponent>AQAB</Exponent></RSAPublicKey>";
-                Chilkat.Global glob = new Chilkat.Global();
-                glob.UnlockBundle("anything for 30-day trial");
-                Chilkat.Jwt jwt = new Chilkat.Jwt();
-                string token = httpContext.Request.Headers["token"].ToString();
-                using (var client = new ConsulClient())
+                if (httpContext.Request.Headers.ContainsKey("token"))
                 {
-                    client.Config.Address = new Uri("35.221.125.153:8500");
-                    var getPair = await client.KV.Get("publickey");
-                    Chilkat.Rsa rsaPublicKey = new Chilkat.Rsa();
-                    rsaPublicKey.ImportPublicKey(Encoding.UTF8.GetString(getPair.Response.Value));
-                    var isTokenVerified = jwt.VerifyJwtPk(token, rsaPublicKey.ExportPublicKeyObj());
-                    if (isTokenVerified)
+                    // string publickey = "<RSAPublicKey><Modulus>5QooqrD6rqxdXTH7XxLYm0rF2uLFTZZAqv1lMcRvNEr7kmvkNEN+NH6soK1/1v5DI6xHwKAHKyi323XrjH7jIrkjHl5RF09nicutCvL0cFiwSCTfWzmIwbAI3z4J09b2lzml44IautLjk18LnRM4fZbpZdzmkd/UmvgOzREvxUU=</Modulus><Exponent>AQAB</Exponent></RSAPublicKey>";
+                    Chilkat.Global glob = new Chilkat.Global();
+                    glob.UnlockBundle("anything for 30-day trial");
+                    Chilkat.Jwt jwt = new Chilkat.Jwt();
+                    string token = httpContext.Request.Headers["token"].ToString();
+                    using (var client = new ConsulClient())
                     {
-                        ResponseHeaders decodedheaders = JsonConvert.DeserializeObject<ResponseHeaders>(jwt.GetPayload(httpContext.Request.Headers["token"]));
-                        httpContext.Request.Headers.Add("agentid", decodedheaders.Agentid.ToString());
-                        httpContext.Request.Headers.Add("name", "decodedheaders.name");
-                        httpContext.Request.Headers.Add("profileimageurl", decodedheaders.Profileimageurl);
-                        httpContext.Request.Headers.Add("departmentid", decodedheaders.Organisationid.ToString());
-                        httpContext.Request.Headers.Add("departmentname", decodedheaders.Departmentname);
-                        httpContext.Request.Headers.Add("organisationname", decodedheaders.Organisationname);
-                        httpContext.Request.Headers.Add("email", decodedheaders.Email);
-                        httpContext.Request.Headers.Remove("token");
-                        await _next(httpContext);
+                        client.Config.Address = new Uri("35.221.125.153:8500");
+                        var getPair = await client.KV.Get("publickey");
+                        Chilkat.Rsa rsaPublicKey = new Chilkat.Rsa();
+                        rsaPublicKey.ImportPublicKey(Encoding.UTF8.GetString(getPair.Response.Value));
+                        var isTokenVerified = jwt.VerifyJwtPk(token, rsaPublicKey.ExportPublicKeyObj());
+                        if (isTokenVerified)
+                        {
+                            ResponseHeaders decodedheaders = JsonConvert.DeserializeObject<ResponseHeaders>(jwt.GetPayload(httpContext.Request.Headers["token"]));
+                            httpContext.Request.Headers.Add("agentid", decodedheaders.Agentid.ToString());
+                            httpContext.Request.Headers.Add("name", "decodedheaders.name");
+                            httpContext.Request.Headers.Add("profileimageurl", decodedheaders.Profileimageurl);
+                            httpContext.Request.Headers.Add("departmentid", decodedheaders.Organisationid.ToString());
+                            httpContext.Request.Headers.Add("departmentname", decodedheaders.Departmentname);
+                            httpContext.Request.Headers.Add("organisationname", decodedheaders.Organisationname);
+                            httpContext.Request.Headers.Add("email", decodedheaders.Email);
+                            httpContext.Request.Headers.Remove("token");
+                            await _next(httpContext);
+                        }
+                        else
+                        {
+                            httpContext.Response.Headers.Add("error", "NotAuthorised");
+                            httpContext.Response.StatusCode = 401;
+                            throw new UnauthorizedAccessException();
+
+                        }
                     }
-                    else
+                }
+                else
+                {
+                    if (httpContext.Request.Path.ToString() != "/login" &&
+                        httpContext.Request.Path.ToString() != "/upload" &&
+                        httpContext.Request.Path.ToString() != "/signup" &&
+                        httpContext.Request.Path.ToString() != "/endusers" &&
+                        httpContext.Request.Path.ToString() != "/agents")
                     {
+
                         httpContext.Response.Headers.Add("error", "NotAuthorised");
                         httpContext.Response.StatusCode = 401;
                         throw new UnauthorizedAccessException();
-
                     }
+                    await _next(httpContext);
                 }
             }
             else
             {
-                if (httpContext.Request.Path.ToString() != "/login" &&
-                    httpContext.Request.Path.ToString() != "/upload" &&
-                    httpContext.Request.Path.ToString() != "/signup" &&
-                    httpContext.Request.Path.ToString() != "/endusers"&&
-                    httpContext.Request.Path.ToString() != "/agents")
-                {
-                    
-                    httpContext.Response.Headers.Add("error", "NotAuthorised");
-                    httpContext.Response.StatusCode = 401;
-                    throw new UnauthorizedAccessException();
-                }
-                await _next(httpContext);
+                httpContext.Response.Headers.Add("error", "NotAuthorised");
+                httpContext.Response.StatusCode = 401;
+                throw new UnauthorizedAccessException();
             }
         }
+       // Extension method used to add the middleware to the HTTP request pipeline.
+
     }
-        // Extension method used to add the middleware to the HTTP request pipeline.
-        public static class AuthenticationExtensions
+    public static class AuthenticationExtensions
+    {
+        public static IApplicationBuilder UseAuthentication(this IApplicationBuilder builder)
         {
-            public static IApplicationBuilder UseAuthentication(this IApplicationBuilder builder)
-            {
-                return builder.UseMiddleware<Authentication>();
-            }
+            return builder.UseMiddleware<Authentication>();
         }
     }
+}
---
+++ ApiGateway/sample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiGateway
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class sample
+    {
+        private readonly RequestDelegate _next;
+
+        public sample(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            //code
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class sampleExtensions
+    {
+        public static IApplicationBuilder Usesample(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<sample>();
+        }
+    }
+}
